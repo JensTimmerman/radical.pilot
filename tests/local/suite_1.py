b7b811d159ea6f8b1fa6ad73bddb0aba88e41f10
AntonsT <antons.treikalis@rutgers.edu>
2015-07-30 00:14:42 -0400

    finalize local tests

--- tests/local/suite_1.py
+++ tests/local/suite_1.py
@@ -403,4 +403,44 @@ def test_state_history(rp_setup_state):
     assert (rp.ACTIVE         in states)
     assert (rp.CANCELED       in states)
 
-    
+#-------------------------------------------------------------------------------
+#
+def test_session_ctx():
+
+    s1 = None
+    s2 = None
+
+    try:
+        s1 = rp.Session(database_url=db_url, 
+                        database_name='rp-testing')
+        
+        print "Session 1: %s (%d)" % (s1.uid, len(s1.list_contexts()))
+  
+        c1 = rp.Context('ssh')
+        c1.user_id = "tg802352"
+        print 'context 1: %s' % c1
+        s1.add_context(c1)
+  
+        c2 = rp.Context('ssh')
+        c2.user_id = "abcedesds"
+        print 'context 2: %s' % c2
+        s1.add_context(c2)
+  
+        for c in s1.list_contexts():
+            print c
+  
+        s2 = rp.Session(uid=s1.uid)
+        print "Session 2: %s (%d)" % (s2.uid, len(s2.list_contexts()))
+  
+        for c in s2.list_contexts():
+            print c
+  
+        assert (len(s1.list_contexts()) == len(s2.list_contexts()))
+
+    except Exception as e:
+        raise
+
+    finally:
+        if s1: s1.close() 
+        if s2: s2.close()
+
